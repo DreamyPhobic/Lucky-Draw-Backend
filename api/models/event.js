e194db060d72676a43a965c1ed7b277b6e3d55e2
Harsh Gupta <33078171+DreamyPhobic@users.noreply.github.com>
2021-04-24 22:42:45 +0530

    Update event.js

--- api/models/event.js
+++ api/models/event.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const eventSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     Reward: { type: String, required: true },
-    Time: { type: Number, required: true },
+    Time: { type: String, required: true },
     Status: { type: Number, default: 0 },
     Participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     Winner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
